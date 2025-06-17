ef005fe624449fcf3c3a2d9aa7c03aaf52505288
KareemSab278 <10288694@student.walsallcollege.ac.uk>
2025-06-17 22:27:59 +0100

    deploying live test - 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import './styles/App.css';
 import BookingsTable from './components/BookingsTable.tsx';
 import CreateBooking from './components/CreateBooking.tsx';
