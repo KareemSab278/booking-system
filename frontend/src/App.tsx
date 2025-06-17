1f06f2d0a90a04eca000e4f096d0bbfa934c090d
KareemSab278 <10288694@student.walsallcollege.ac.uk>
2025-06-17 22:15:14 +0100

    prepped for live - ready (added some instructions on the page)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,13 @@ function App() {
   return (
     <>
       <div>
-        
+        <h1>Hotel Booking System</h1>
+      Welcome to the Hotel Booking POS System. You can create, view, update, and delete bookings.
       <BookingsTable />
+      To delete, click on a booking ID and press the delete key. To update, click on a field and edit the field then press enter.<br />
+      Use the form below to create a new booking, and the table above shows existing bookings.
       <CreateBooking />
+      (This service is not meant to work on mobile devices yet)
       </div>
     </>
   )
