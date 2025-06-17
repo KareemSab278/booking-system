addc0ee9c515e639fc6dc456d59ccee09fccad03
KareemSab278 <10288694@student.walsallcollege.ac.uk>
2025-06-17 22:05:32 +0100

    prepping for live - forgot to open port

--- backend/api.py
+++ backend/api.py
@@ -155,6 +155,8 @@ def delete_booking(id):
 #================================== END ===============================
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True) #had to add this to make it work in render lol
+
 
 #=============================================================
